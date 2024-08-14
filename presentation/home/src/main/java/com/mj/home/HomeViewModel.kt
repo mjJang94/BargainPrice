2ee5a36171cd404b3d4582f58623aae8926fd894
mjJang94 <alswhddl10@naver.com>
2024-08-14 16:09:56 +0900

    PagingData 즐겨찾기 기능 레퍼런스 기록

--- presentation/home/src/main/java/com/mj/home/HomeViewModel.kt
+++ presentation/home/src/main/java/com/mj/home/HomeViewModel.kt
@@ -10,7 +10,10 @@ import com.mj.domain.usecase.shopping.CombinedShoppingUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -43,6 +46,8 @@ class HomeViewModel @Inject constructor(
             combinedShoppingUseCases.getShoppingData(dispatcher = Dispatchers.IO, param = query)
                 .distinctUntilChanged()
                 .cachedIn(viewModelScope)
+                //https://medium.com/@vagabond95/paging-3-%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-%EC%83%81%ED%83%9C-%EB%B3%80%EA%B2%BD-15400cedf423
+                //.combine(_shoppingInfo) {}
                 .collect {
                     _shoppingInfo.emit(it)
                     setState { copy(shoppingInfo = _shoppingInfo, isLoading = false) }
