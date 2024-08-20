82d8a61d89b5b374cc79b2fcdbdf4a8327d33190
mjJang94 <alswhddl10@naver.com>
2024-08-20 11:24:07 +0900

    NotificationType 리팩토링

--- core/src/main/java/com/mj/core/notification/NotificationType.kt
+++ core/src/main/java/com/mj/core/notification/NotificationType.kt
@@ -1,9 +1,35 @@
 package com.mj.core.notification
 
 import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import com.mj.core.R
+import com.mj.core.flags
 
-sealed class NotificationType {
-    data class Simple(val title: String, val message: String) : NotificationType()
-    data class Action(val title: String, val message: String, val actionText: String, val actionIntent: PendingIntent) : NotificationType()
-    data class Progress(val title: String, val message: String, val progress: Int) : NotificationType()
+sealed class NotificationType(
+    val title: String,
+    val message: String,
+    val actionText: String?,
+    val pendingIntent: PendingIntent?,
+) {
+    data class RefreshSuccess(
+        private val context: Context,
+        private val intent: Intent,
+    ) : NotificationType(
+        title = context.getString(R.string.noti_refresh_success_title),
+        message = context.getString(R.string.noti_refresh_success_label),
+        actionText = context.getString(R.string.noti_refresh_success_confirm),
+        pendingIntent = PendingIntent.getActivity(context, 0, intent, flags()),
+    )
+
+    data class RefreshFailure(
+        private val context: Context,
+        private val reason: String,
+    ) : NotificationType(
+        title = context.getString(R.string.noti_refresh_success_title),
+        message = reason,
+        actionText = null,
+        pendingIntent = null,
+    )
 }
+
