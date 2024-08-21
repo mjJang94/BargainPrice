41f595341841bc0c55ee9be720714f42fe137f13
mjJang94 <alswhddl10@naver.com>
2024-08-22 08:45:25 +0900

    Glide 라이브러리 경고 log 대응 처리

---
+++ core/src/main/java/com/mj/core/common/GlideModule.kt
@@ -0,0 +1,14 @@
+package com.mj.core.common
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule: AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+    }
+}
