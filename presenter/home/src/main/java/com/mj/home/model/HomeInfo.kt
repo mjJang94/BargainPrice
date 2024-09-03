44e41be4dbaf79fd009ab5a586004384b7e2c642
mjJang94 <alswhddl10@naver.com>
2024-09-03 16:16:42 +0900

    Presenter -> presentation 교체

--- presenter/home/src/main/java/com/mj/home/model/HomeInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.mj.home.model
-
-import androidx.paging.PagingData
-import com.mj.domain.model.Shopping
-
-data class HomeInfo(
-
-    val shopping: PagingData<Shopping>
-)
--- presenter/home/src/main/java/com/mj/home/model/Pages.kt
+++
@@ -1,8 +0,0 @@
-package com.mj.home.model
-
-import com.mj.home.R
-
-enum class Pages(val resId: Int) {
-    SEARCH(resId = R.string.search),
-    FAVORITE(resId = R.string.favorite),
-}
